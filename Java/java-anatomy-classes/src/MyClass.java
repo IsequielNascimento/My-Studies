bfccc62e4a0d01a08ed0739db4964dce79ed0989
IsequielNascimento <isequielnascimento@gmail.com>
2023-02-10 09:52:14 -0300

    Class Anatomy - Class 2

--- Java/java-anatomy-classes/src/MyClass.java
+++ Java/java-anatomy-classes/src/MyClass.java
@@ -1,6 +1,10 @@
 public class MyClass {
-    
-    public static void main(String [] args) {
-        System.out.print ("Hello World!");
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+
+        int numero = 10;
+        String nome = "Rafael";
+        final double PI = 3.14;
     }
 }
