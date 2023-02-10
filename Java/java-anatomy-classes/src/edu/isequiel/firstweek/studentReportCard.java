1740404cca68fbed808b8ae95c7e16bd16dc833d
IsequielNascimento <isequielnascimento@gmail.com>
2023-02-10 10:38:15 -0300

    Class Anatomy - Class 4

---
+++ Java/java-anatomy-classes/src/edu/isequiel/firstweek/studentReportCard.java
@@ -0,0 +1,14 @@
+package edu.isequiel.firstweek;
+
+public class studentReportCard {
+    
+    public static void main (String[] args) {
+        
+        int finalMedia = 5;
+        if (finalMedia >= 7) {
+            System.out.println("Approved");
+        } else {
+            System.out.println("Disapproved");
+        }
+    }
+}
